b64bbc5a88a3c240fe1790602a6b303658ba96f9
Turbo Laser <galiakberovigor@gmail.com>
2021-09-06 03:08:28 +0300

    modal window class

--- interface.py
+++ interface.py
@@ -1,3 +1,5 @@
+import os
+
 import pygame
 
 import global_vars
@@ -5,7 +7,7 @@ import utils
 
 
 class Button:
-	def __init__(self, path, size, position):
+	def __init__(self, path, size, position, text=None):
 		self.button_normal = utils.get_adopted_image(path + '_normal', size)
 		self.button_hover = utils.get_adopted_image(path + '_hover', size)
 		self.button_pushed = utils.get_adopted_image(path + '_pushed', (
@@ -17,6 +19,11 @@ class Button:
 		}
 		self._is_pushed = False
 		self.sound = global_vars.BUTTON_SOUND
+		if text is not None:
+			self.text = global_vars.font.render(text, True, global_vars.BLACK)
+			self.text_position = self.text.get_rect(center=(size[0] // 2, size[1] // 2))
+			for surface in self.states_images.values():
+				surface[0].blit(self.text, self.text_position)
 
 	def is_pushed(self):
 		return bool(self._is_pushed)
@@ -35,12 +42,10 @@ class Button:
 
 class Interface:
 	def __init__(self):
-		self.end_turn_button = Button('assets/button', (
-			int(14 * global_vars.RATIO), int(5 * global_vars.RATIO)),
-			(global_vars.RATIO * 10.1, global_vars.RATIO * 2.4))
-		self.next_phase_button = Button('assets/button1', (
-			int(14 * global_vars.RATIO), int(5 * global_vars.RATIO)),
-			(global_vars.RATIO * 10.1, global_vars.RATIO * 6.6))
+		self.end_turn_button = \
+			Button('assets/button', global_vars.BUTTON_SIZE, (global_vars.RATIO * 10.1, global_vars.RATIO * 2.4))
+		self.next_phase_button = \
+			Button('assets/button1', global_vars.BUTTON_SIZE, (global_vars.RATIO * 10.1, global_vars.RATIO * 6.6))
 		self.main_surface = pygame.Surface((global_vars.RATIO * 20.2, global_vars.RATIO * 21))
 		self.main_surface_position = self.main_surface.get_rect(
 			topleft=(3 * global_vars.RATIO, 3.3 * global_vars.RATIO))
@@ -82,4 +87,74 @@ class Interface:
 		else:
 			self.control_surface.blit(*button.states_images.get('normal'))
 			button.release()
+
+
+class ModalWindow:
+	def __init__(self, buttons_text, text, size, picture=None):
+		pygame.image.save(global_vars.window, 'assets/screenshot' + global_vars.FORMAT)
+		self.screenshot = pygame.image.load('assets/screenshot' + global_vars.FORMAT)
+		self.screenshot.set_alpha(160)
+		self.surface = utils.get_adopted_image(global_vars.MODAL_WINDOW, size)
+		self.surface_position = self.surface.get_rect(center=global_vars.MIDDLE_POINT)
+		self.color_surface = pygame.Surface((global_vars.MODAL_SIZE[0] / 1.05, global_vars.MODAL_SIZE[1] / 1.06))
+		self.color_surface_position = self.color_surface.get_rect(center=(size[0] / 2, size[1] / 2))
+		self.text = global_vars.font.render(text, True, global_vars.BLACK)
+		self.text_position = self.text.get_rect(center=(size[0] / 2, size[1] / 1.4))
+		self.size = size
+		self.is_alive = True
+		self.buttons = list()
+		self.pushed_button = None
+		buttons_amount = len(buttons_text)
+		for i in range(buttons_amount):
+			b_pos = (size[0] // (buttons_amount + 1) + global_vars.BUTTON_SIZE[0] * i, size[1] - global_vars.RATIO * 6)
+			self.buttons.append(Button('assets/mbutton', global_vars.BUTTON_SIZE, b_pos, buttons_text[i]))
+		if picture is not None:
+			self.image = utils.get_adopted_image(picture, (int(size[0] / 2), int(size[1] / 2)))
+			self.image_position = self.image.get_rect(center=(size[0] / 2, size[1] / 3))
+		else:
+			self.image = None
+
+	def __del__(self):
+		os.remove('assets/screenshot.png')
+		print('button is dead')
+
+	def draw(self, mouse_position, lmb_is_pressed):
+		global_vars.window.blit(self.screenshot, global_vars.ZERO_POINT)
+		global_vars.window.blit(self.surface, self.surface_position)
+		self.surface.blit(self.color_surface, self.color_surface_position)
+		self.color_surface.fill(global_vars.INFO_COLOR)
+		self.color_surface.blit(self.text, self.text_position)
+		if self.image is not None:
+			self.color_surface.blit(self.image, self.image_position)
+		for button in self.buttons:
+			cursor_is_on_button = button.states_images.get('hover')[1].collidepoint(
+				mouse_position[0] - self.surface_position.topleft[0],
+				mouse_position[1] - self.surface_position.topleft[1])
+			if cursor_is_on_button and not lmb_is_pressed:
+				self.color_surface.blit(*button.states_images.get('hover'))
+			elif cursor_is_on_button and lmb_is_pressed:
+				self.color_surface.blit(*button.states_images.get('pushed'))
+			else:
+				self.color_surface.blit(*button.states_images.get('normal'))
+
+	def modal_window_process(self):
+		clock = pygame.time.Clock()
+		while self.is_alive:
+			pygame.event.get()
+			mouse_position = pygame.mouse.get_pos()
+			mouse_buttons = pygame.mouse.get_pressed(3)
+			global_vars.window.fill(global_vars.BLACK)
+			self.draw(mouse_position, mouse_buttons[0])
+			pygame.display.update()
+			clock.tick(30)
+			for i in range(len(self.buttons)):
+				cursor_is_on_button = self.buttons[i].states_images.get('hover')[1].collidepoint(
+					mouse_position[0] - self.surface_position.topleft[0],
+					mouse_position[1] - self.surface_position.topleft[1])
+				if cursor_is_on_button and mouse_buttons[0]:
+					self.pushed_button = i
+					self.is_alive = False
+		return self.pushed_button
+
+
 
